acb8a603d47ac105a4e99e46ec716ebeefd5e825
LucaMarche IZ1MLT <lucamarche-iz1mlt@users.noreply.github.com>
2018-05-16 10:04:51 +0200

    bug fix

--- install/ircddbgateway.sh
+++ install/ircddbgateway.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 PATH_RUN_SCRIPT=$1
-LOG_PATH_DMRGATEWAY=$2
-CONFIG_PATH_DMRGATEWAY=$3
+LOG_PATH_IRCDDBGATEWAY=$2
+CONFIG_PATH_IRCDDBGATEWAY=$3
 PATH_EXEC=$4
 N_CPU=$5
 
@@ -21,7 +21,7 @@ N_CPU=$5
 	mkdir -p ${LOG_PATH_IRCDDBGATEWAY}
 	chmod -R 777 ${CONFIG_PATH_IRCDDBGATEWAY}
 	chmod -R 777 ${LOG_PATH_IRCDDBGATEWAY}
-	cp -R ${PATH_RUN_SCRIPT}ircddbgateway ${CONFIG_PATH_IRCDDBGATEWAY}
+	cp -R ${PATH_RUN_SCRIPT}/ircddbgateway ${CONFIG_PATH_IRCDDBGATEWAY}
 	cp -R ${PATH_RUN_SCRIPT}/service/ircddbgatewayd.service /lib/systemd/system/
 	cp -R ${PATH_RUN_SCRIPT}/service/ircddbgatewayd.timer /lib/systemd/system/
 	chmod 755 /lib/systemd/system/ircddbgatewayd.service
