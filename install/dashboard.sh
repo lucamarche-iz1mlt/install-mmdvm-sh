544e82c315d0df6d9da193b316ae36a96efcac6e
LucaMarche IZ1MLT <lucamarche-iz1mlt@users.noreply.github.com>
2018-05-31 11:56:38 +0200

    Added installation missing packages

--- install/dashboard.sh
+++ install/dashboard.sh
@@ -9,6 +9,9 @@
 	chown -R www-data:www-data /var/www/html
 	chmod -R 775 /var/www/html
 	
+	apt-get install php7.0-common php7.0-cgi php
+	apt-get install sqlite3 php7.0-sqlite
+	
 	lighty-enable-mod fastcgi
 	lighty-enable-mod fastcgi-php
 	service lighttpd force-reload
