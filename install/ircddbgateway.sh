0ffae699a0eee686225ad0e87102209410b226cd
Luca Marchesano <lucamarche-iz1mlt@users.noreply.github.com>
2018-08-24 16:27:56 +0200

    Add new ircddbgateway

--- install/ircddbgateway.sh
+++ install/ircddbgateway.sh
@@ -6,22 +6,24 @@ CONFIG_PATH_IRCDDBGATEWAY=$3
 PATH_EXEC=$4
 N_CPU=$5
 
-	git clone https://github.com/dl5di/OpenDV.git /home/pi/MMDVM/OpenDV
+	git clone https://github.com/g4klx/ircDDBGateway.git /home/pi/MMDVM/
 	echo 'Compilazione  IRCDDBGateway...'
-	cd /home/pi/MMDVM/OpenDV/ircDDBGateway/
+	cd /home/pi/MMDVM/ircDDBGateway/
     make clean
-	./configure --without-gui
+	sed -i "s%/var/log%${LOG_PATH_IRCDDBGATEWAY}%g" /home/pi/MMDVM/ircDDBGateway/Makefile
+	sed -i "s%/etc%${CONFIG_PATH_IRCDDBGATEWAY}%g" /home/pi/MMDVM/ircDDBGateway/Makefile
+	sed -i "s%/usr/bin%/usr/local/bin%g" /home/pi/MMDVM/ircDDBGateway/Makefile
 	if [ $N_CPU = "0" ]; then
-		sudo make
+		make
 	else
-		sudo make -j$N_CPU all
+		make -j$N_CPU all
 	fi
 
 	make install
 	mkdir -p ${CONFIG_PATH_IRCDDBGATEWAY}
 	mkdir -p ${LOG_PATH_IRCDDBGATEWAY}
-	chmod -R 777 ${CONFIG_PATH_IRCDDBGATEWAY}
-	chmod -R 777 ${LOG_PATH_IRCDDBGATEWAY}
+	chown -R pi ${CONFIG_PATH_IRCDDBGATEWAY}
+	chown -R pi ${LOG_PATH_IRCDDBGATEWAY}
 	cp -R ${PATH_RUN_SCRIPT}/ircddbgateway ${CONFIG_PATH_IRCDDBGATEWAY}
 	cp -R ${PATH_RUN_SCRIPT}/service/ircddbgatewayd.service /lib/systemd/system/
 	cp -R ${PATH_RUN_SCRIPT}/service/ircddbgatewayd.timer /lib/systemd/system/
